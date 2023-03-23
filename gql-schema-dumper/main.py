0b80308ac693cd6d47de0f4f75b07f64db84f120
Shogo Sawai <shogo.sawai@gmail.com>
2023-03-23 09:11:36 +0900

    add interval update feature

--- gql-schema-dumper/main.py
+++ gql-schema-dumper/main.py
@@ -1,6 +1,7 @@
 import hashlib
 import json
 import time
+import os
 from typing import Optional
 
 import click
@@ -18,15 +19,19 @@ def output_schema_str(
         url,
         headers={"Content-Type": "application/json"},
         data=json.dumps({"query": get_introspection_query()}),
+        timeout=(30, 60),
     )
     schema_str = print_schema(build_client_schema(res.json()["data"]))
     digest = hashlib.sha256(schema_str.encode("utf-8")).hexdigest()
     if digest != olddigest:
         if output:
-            with open(output, "rb") as fp:
+            print(f"digest changed {output} update")
+            with open(output, "wb") as fp:
                 fp.write(schema_str.encode("utf-8"))
         else:
             print(schema_str)
+    else:
+        print(f"no changes found for {output}")
 
     return digest
 
@@ -37,22 +42,42 @@ def output_schema_str(
 @click.option("--watch", help="watch path. ',' separated path list")
 @click.option("--watch-delay-ms", default=1000, help="delay after file changes.")
 @click.option("--yield-on-timeout", default=False, is_flag=True)
-def dump_schema(url: str, output: str, watch: str, watch_delay_ms: int, yield_on_timeout: bool):
+@click.option("--polling-interval", type=float)
+@click.option("--verbose", type=bool)
+def dump_schema(
+    url: str,
+    output: str,
+    watch: str,
+    watch_delay_ms: int,
+    yield_on_timeout: bool,
+    polling_interval: float = -1,
+        verbose: bool = False
+):
     session = requests.Session()
     adapter = HTTPAdapter(
         pool_connections=1,
         pool_maxsize=1,
-        max_retries=Retry(total=30, backoff_factor=0.5, status_forcelist=[500, 502, 503, 504]),
+        max_retries=Retry(connect=10, read=5, total=30, backoff_factor=0.5, status_forcelist=[500, 502, 503, 504]),
     )
     session.mount("http://", adapter)
     session.mount("https://", adapter)
 
-    digest = output_schema_str(session, url, output)
+    if os.path.exists(output):
+        with open(output, "rb") as fp:
+            digest = hashlib.sha256(fp.read()).hexdigest()
+    else:
+        digest = None
+
+    digest = output_schema_str(session, url, output, olddigest=digest)
 
     if watch:
         for _ in watchfiles.watch(*watch.split(","), yield_on_timeout=yield_on_timeout):
             if watch_delay_ms:
                 time.sleep(watch_delay_ms / 1000)
+            digest = output_schema_str(session, url, output, olddigest=digest)
+    elif polling_interval > 0:
+        while True:
+            time.sleep(polling_interval)
             digest = output_schema_str(session, url, output, olddigest=digest)
 
 
