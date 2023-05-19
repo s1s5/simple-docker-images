533c664619f661e1764ae8d4a80b976841059b04
Shogo Sawai <shogo.sawai@gmail.com>
2023-05-19 11:07:42 +0900

    bug fix

--- rust-dev/entrypoint.sh
+++ rust-dev/entrypoint.sh
@@ -4,10 +4,17 @@
 set -eu  # <= 0以外が返るものがあったら止まる, 未定義の変数を使おうとしたときに打ち止め
 
 
-groupadd -g $GROUP_ID $GROUP_NAME
-useradd -m -s /bin/bash -u $USER_ID -g $GROUP_ID $USER_NAME
+if getent group "$GROUP_NAME" >/dev/null; then
+    :
+else
+    groupadd -g $GROUP_ID $GROUP_NAME
+    useradd -m -s /bin/bash -u $USER_ID -g $GROUP_ID $USER_NAME
 
-mkdir -p /home/${USER_NAME}
+    mkdir -p /home/${USER_NAME}
+    mkdir -p /home/${USER_NAME}/.cargo
+    chown -R ${USER_NAME}:${GROUP_NAME} /home/${USER_NAME}
+fi
 export HOME=/home/${USER_NAME}
+export CARGO_HOME=${HOME}/.cargo
 
 exec gosu $USER_NAME "$@"
