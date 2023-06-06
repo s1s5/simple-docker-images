433d55f0bdb8ff8a20450f3a2c5682cea9216025
Shogo Sawai <shogo.sawai@gmail.com>
2023-06-06 11:01:55 +0900

    fixed user, group

--- rust-dev/entrypoint.sh
+++ rust-dev/entrypoint.sh
@@ -4,17 +4,13 @@
 set -eu  # <= 0以外が返るものがあったら止まる, 未定義の変数を使おうとしたときに打ち止め
 
 
-if getent group "$GROUP_NAME" >/dev/null; then
+if getent group dev >/dev/null; then
     :
 else
-    groupadd -g $GROUP_ID $GROUP_NAME
-    useradd -m -s /bin/bash -u $USER_ID -g $GROUP_ID $USER_NAME
+    groupadd -g $GROUP_ID dev
+    useradd -m -s /bin/bash -u $USER_ID -g $GROUP_ID dev
 
-    mkdir -p /home/${USER_NAME}
-    mkdir -p /home/${USER_NAME}/.cargo
-    chown -R ${USER_NAME}:${GROUP_NAME} /home/${USER_NAME}
+    chown -R dev:dev /home/dev
 fi
-export HOME=/home/${USER_NAME}
-export CARGO_HOME=${HOME}/.cargo
 
-exec gosu $USER_NAME "$@"
+exec gosu dev "$@"
