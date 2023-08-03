430e90f1592aaa4a3b07d82555d8c1d4f73a9abc
Shogo Sawai <shogo.sawai@gmail.com>
2023-08-03 16:19:00 +0900

    add tag

--- build.sh
+++ build.sh
@@ -5,7 +5,10 @@ set -eu  # <= 0以外が返るものがあったら止まる, 未定義の変数
 
 
 for foldar in dropbox-download dropbox-upload gpg-encrypt-asym gpg-encrypt-sym gql-schema-dumper rover-supergraph-compose s3-download-to-tar http-debugger rust-dev tcp-logger http-echo-logger node-watchfiles s3-download-latest; do
+    export TAG=`date "+%Y%m%d-%H%M%S"`
     docker build ${foldar} -t s1s5/${foldar}
+    docker tag s1s5/${foldar} s1s5/${foldar}:${TAG}
+    docker push s1s5/${foldar}:${TAG}
     docker push s1s5/${foldar}
 done
 
